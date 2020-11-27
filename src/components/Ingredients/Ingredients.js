7602df4ae99f790a5239e7688a8ace146789e4e5
Starlight Romero <starlightromero@gmail.com>
2020-11-27 15:22:40 -0800

    added useEffect hook to get ingredients from database

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import api from '../../api'
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
@@ -6,6 +6,22 @@ import Search from './Search'
 
 const Ingredients = () => {
   const [ ingredients, setIngredients ] = useState([])
+
+  useEffect(() => {
+    api.get('ingredients.json').then(response => {
+      const loadedIngredients = []
+      for (const key in response.data) {
+        loadedIngredients.push({
+          id: key,
+          title: response.data[key].title,
+          amount: response.data[key].amount
+        })
+      }
+      setIngredients(loadedIngredients)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
 
   const addIngredientHandler = ingredient => {
     api.post('ingredients.json', ingredient).then(response => {
