b1b125cbe4b4dbf3f9e07e0777a85848d61dd12d
Starlight Romero <starlightromero@gmail.com>
2020-11-27 20:56:35 -0800

    added ErrorModal and sate fir error

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,28 +3,37 @@ import api from '../../api'
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
 import Search from './Search'
+import ErrorModal from '../UI/ErrorModal'
 
 const Ingredients = () => {
   const [ ingredients, setIngredients ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(false)
+  const [ error, setError ] = useState()
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true)
     api.post('ingredients.json', ingredient).then(response => {
       setIngredients(prevIngredients => [
         ...prevIngredients,
         { id: response.data.name, ...ingredient }
       ])
+      setIsLoading(false)
     }).catch(error => {
-      console.log(error)
+      setIsLoading(false)
+      setError('Something went wrong!')
     })
   }
 
   const removeIngredientHandler = ingredientId => {
+    setIsLoading(true)
     api.delete(`ingredients/${ingredientId}.json`).then(response => {
       setIngredients(prevIngredients =>
         prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       )
+      setIsLoading(false)
     }).catch(error => {
-      console.log(error)
+      setIsLoading(false)
+      setError('Something went wrong!')
     })
   }
 
@@ -32,9 +41,14 @@ const Ingredients = () => {
     setIngredients(filteredIngredients)
   }, [])
 
+  const clearError = () => {
+    setError(null)
+  }
+
   return (
     <div className='App'>
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
         <Search onLoadIngredients={filterIngredientsHandler} />
