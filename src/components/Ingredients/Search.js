7a4e48e520d604bc67a3c9567cf5b12410df5ac3
Starlight Romero <starlightromero@gmail.com>
2020-11-27 19:22:06 -0800

    added useEffect to get ingredients from backend

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import api from '../../api'
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from '../UI/Card'
+import './Search.css'
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props
+  const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`
+    api.get(`ingredients.json${query}`).then(response => {
+      const loadedIngredients = []
+      for (const key in response.data) {
+        loadedIngredients.push({
+          id: key,
+          title: response.data[key].title,
+          amount: response.data[key].amount
+        })
+      }
+      onLoadIngredients(loadedIngredients)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [filter, onLoadIngredients])
+
   return (
-    <section className="search">
+    <section className='search'>
       <Card>
-        <div className="search-input">
+        <div className='search-input'>
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type='text'
+            value={filter}
+            onChange={event => setFilter(event.target.value)} />
         </div>
       </Card>
     </section>
-  );
-});
+  )
+})
 
-export default Search;
+export default Search
