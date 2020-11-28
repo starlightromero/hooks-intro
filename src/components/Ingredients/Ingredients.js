6387dcaf1e99204fe874b6128581dbcba9d0d6dc
Starlight Romero <starlightromero@gmail.com>
2020-11-28 15:17:29 -0800

    added useHttp hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,9 +1,10 @@
-import React, { useReducer, useCallback, useMemo } from 'react'
+import React, { useReducer, useCallback, useMemo, useEffect } from 'react'
 import api from '../../api'
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
 import Search from './Search'
 import ErrorModal from '../UI/ErrorModal'
+import useHttp from '../../hooks/http'
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
@@ -18,44 +19,36 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 }
 
-const httpReducer = (httpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return { loading: true, error: null }
-    case 'RESPONSE':
-      return { loading: false, error: null }
-    case 'ERROR':
-      return { loading: false, error: action.error }
-    case 'CLEAR':
-      return { ...httpState, error: null }
-    default:
-      throw new Error('Reducer case was not found')
-  }
-}
-
 const Ingredients = () => {
   const [ ingredients, dispatch ] = useReducer(ingredientReducer, [])
-  const [ httpState, dispatchHttp ] = useReducer(httpReducer, { loading: false, error: null })
+  const { isLoading, error, data, sendRequest, extra, identifier } = useHttp()
+
+  useEffect(() => {
+    if (!isLoading && !error && identifier === 'REMOVE_INGREDIENT') {
+      dispatch({type: 'DELETE', id: extra})
+    } else if (!isLoading && !error && identifier === 'ADD_INGREDIENT') {
+      dispatch({ type: 'ADD', ingredient: { id: data.name, ...extra } })
+    }
+  }, [data, error, extra, identifier, isLoading])
 
   const addIngredientHandler = useCallback(ingredient => {
-    dispatchHttp({ type: 'SEND' })
-    api.post('ingredients.json', ingredient).then(response => {
-      dispatch({ type: 'ADD', ingredient: { id: response.data.name, ...ingredient } })
-      dispatchHttp({ type: 'RESPONSE' })
-    }).catch(error => {
-      dispatchHttp({ type: 'ERROR', error: 'Somthing went wrong!' })
+    sendRequest({
+      url: 'ingredients.json',
+      method: 'post',
+      body: ingredient,
+      extra: ingredient,
+      identifier: 'ADD_INGREDIENT'
     })
-  }, [])
+  }, [sendRequest])
 
   const removeIngredientHandler = useCallback(ingredientId => {
-    dispatchHttp({ type: 'SEND' })
-    api.delete(`ingredients/${ingredientId}.json`).then(response => {
-      dispatch({ type: 'DELETE', id: ingredientId})
-      dispatchHttp({ type: 'RESPONSE' })
-    }).catch(error => {
-      dispatchHttp({ type: 'ERROR', error: 'Somthing went wrong!' })
+    sendRequest({
+      url: `ingredients/${ingredientId}.json`,
+      method: 'delete',
+      extra: ingredientId,
+      identifier: 'REMOVE_INGREDIENT'
     })
-  }, [])
+  }, [sendRequest])
 
   const filterIngredientsHandler = useCallback(filteredIngredients => {
     dispatch({ type: 'SET', ingredients: filteredIngredients })
@@ -75,8 +68,8 @@ const Ingredients = () => {
 
   return (
     <div className='App'>
-      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
       <section>
         <Search onLoadIngredients={filterIngredientsHandler} />
         {ingredientList}
