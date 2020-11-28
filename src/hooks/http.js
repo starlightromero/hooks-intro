3c907e882aee2ef73414affc16dad5d627e5d1e4
Starlight Romero <starlightromero@gmail.com>
2020-11-28 15:17:13 -0800

    added custom http hook

---
+++ src/hooks/http.js
@@ -0,0 +1,51 @@
+import { useReducer, useCallback } from 'react'
+import api from '../api'
+
+const httpReducer = (httpState, action) => {
+  const { type, error, data, extra, identifier } = action
+  switch (type) {
+    case 'SEND':
+      return { ...httpState, loading: true, error: null, data: null, extra: null, identifier }
+    case 'RESPONSE':
+      return { ...httpState, loading: false, data, extra }
+    case 'ERROR':
+      return { ...httpState, loading: false, error }
+    case 'CLEAR':
+      return { ...httpState, error: null }
+    default:
+      throw new Error('Reducer case was not found')
+  }
+}
+
+const useHttp = () => {
+  const [ httpState, dispatchHttp ] = useReducer(
+    httpReducer,
+    { loading: false, error: null, data: null, extra: null, identifier: null }
+  )
+
+  const sendRequest = useCallback(({ url, method, body, extra, identifier }) => {
+    dispatchHttp({ type: 'SEND', identifier })
+    api({
+      method,
+      url,
+      data: body
+    }).then(response => {
+      dispatchHttp({ type: 'RESPONSE', data: response.data, extra })
+    }).catch(error => {
+      dispatchHttp({ type: 'ERROR', error: 'Somthing went wrong!' })
+    })
+  }, [])
+
+  const { loading, error, data, extra, identifier } = httpState
+
+  return {
+    isLoading: loading,
+    error,
+    data,
+    extra,
+    identifier,
+    sendRequest: sendRequest
+  }
+}
+
+export default useHttp
